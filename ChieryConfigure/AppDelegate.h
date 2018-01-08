3797c8e6e0bc676301f5400f531d75884f6fb86a
handong.wang <handong.wang@qunar.com>
2018-01-08 15:06:05 +0800

    添加资源文件

---
+++ ChieryConfigure/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  ChieryConfigure
+//
+//  Created by chiery on 2018/1/8.
+//  Copyright © 2018年 com.chiery.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ChieryConfigure/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  ChieryConfigure
+//
+//  Created by chiery on 2018/1/8.
+//  Copyright © 2018年 com.chiery.com. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    
+    return YES;
+}
+
+@end
